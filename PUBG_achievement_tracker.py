c0a98eb5f70c7d7e0bac68e6b434e4a18dafadb5
Jasper Nelligan <japperan123@gmail.com>
2020-07-19 19:53:25 -0600

    Buttons now flicker red when clicked

--- PUBG_achievement_tracker.py
+++ PUBG_achievement_tracker.py
@@ -10,14 +10,17 @@ from PIL import Image, ImageTk
 
 # Code structure referenced from: https://tinyurl.com/y3g9ab5q
 
+# Helpful links:
+# 
+# Tkinter events and bindings:
+# http://effbot.org/tkinterbook/tkinter-events-and-bindings.htm
+
 # (C) 2020 Jasper Nelligan
 #**********************************************************************
 
 # Desired sizes for the button images
-OVERVIEW_BUTTON_SIZE = 163, 45
-ACHIEVEMENTS_BUTTON_SIZE = 227, 45
-COMPLETED_BUTTON_SIZE = 163, 45
-CREDITS_BUTTON_SIZE = 163, 45
+BUTTON_SIZE = 277, 45
+
 
 # Most windows screens have 16 pixels that aren't shown on screen
 # ie. on the edges of the screen where the mouses disappear
@@ -103,56 +106,58 @@ class MainMenu(tk.Frame):
         self.controller = controller
 
         # Initialize the background image with buttons/text
-        self.init_image()
+        self.init_images()
 
         #Placing background image onto main menu frame
 
         # Use self as the parent since we are placing 
         # this label onto the frame
-        background_label = tk.Label(self, image=self.tk_background_image)
-        background_label.place(height=BACKGROUND_IMAGE_HEIGHT, 
+        self.main_menu_label = tk.Label(self, image=self.tk_background_image)
+        self.main_menu_label.place(height=BACKGROUND_IMAGE_HEIGHT, 
                                width=1366 - SCREEN_OFFSET)
 
         # Adding functionality to buttons
 
-        background_label.bind('<Button-1>', self. on_click)
+        self.main_menu_label.bind('<Button-1>', self.on_click)
 
-    def on_click(self, event):
-        """Takes action depending on where the user clicked"""
-        print("area clicked was", event.x, event.y, sep=" ")
-        if 75 <= event.x <= 240 and 220 <= event.y <= 265:
-            print("clicked overview")
-        if 75 <= event.x <= 300 and 320 <= event.y <= 365:
-            print("clicked achievements")
-        if 75 <= event.x <= 240 and 420 <= event.y <= 465:
-            print("clicked completed")
-        if 75 <= event.x <= 197 and 520 <= event.y <= 565:
-            print("clicked credits")
-
-    def init_image(self):
+    def init_images(self):
         """Initializes the background image, text, and
         buttons for this frame. 
         """
-
-        #loading main menu images
+        # Loading main menu images
 
         # Colour for text gradients was DEDF00 for yellow
         # and DF0D0D for red, using
         # https://fontmeme.com/playerunknowns-battlegrounds-font/
-        background_image = Image.open('./Images/PUBG_background_image.png')
-        program_title_image = Image.open('./Images/program_title.png')
-        overview_button_image = Image.open('./Images/overview.png')
-        achievements_button_image = Image.open('./Images/achievements.png')
-        completed_button_image = Image.open('./Images/completed.png')
-        credits_button_image = Image.open('./Images/credits.png')
-        # Red text images are for when the user clicks on the buttons,
-        # with the button turning red to indicate a click
-        credits_red_button_image = Image.open('./Images/credits_red.png')
-        overview_red_button_image = Image.open('./Images/overview_red.png')
-        achievements_red_button_image = Image.open('./Images/achievements_red.png')
-        completed_red_button_image = Image.open('./Images/completed_red.png')
-        credits_red_button_image = Image.open('./Images/credits_red.png')
-    
+        # self. is used so that these images are stored
+        # as an instance of this class and can be referenced in other
+        # methods.
+        self.background_image = Image.open('./Images/background.png')
+        self.program_title_image = Image.open('./Images/program_title.png')
+        self.overview_button_image = Image.open('./Images/overview.png')
+        self.achievements_button_image = Image.open('./Images/achievements.png')
+        self.completed_button_image = Image.open('./Images/completed.png')
+        self.credits_button_image = Image.open('./Images/credits.png')
+        # Red buttons will be used to indicate when the user 
+        # has clicked a button. These images are used in the 
+        # change_button_to_red() method
+        self.overview_red_button_image = Image.open('./Images/overview_red.png')
+        self.achievements_red_button_image = Image.open('./Images/achievements_red.png')
+        self.completed_red_button_image = Image.open('./Images/completed_red.png')
+        self.credits_red_button_image = Image.open('./Images/credits_red.png')
+
+        # Shrinking button images
+        for img in (self.overview_button_image, 
+                   self.achievements_button_image,
+                   self.completed_button_image,
+                   self.credits_button_image,
+                   self.overview_red_button_image,
+                   self.achievements_red_button_image,
+                   self.completed_red_button_image,
+                   self.credits_red_button_image):
+            img.thumbnail(BUTTON_SIZE, Image.BICUBIC)
+
+
         # Notes from stackoverflow:
         # How to use .paste():
         # First parameter to .paste() is the image to paste.
@@ -162,24 +167,75 @@ class MainMenu(tk.Frame):
         # then the alpha channel is used as mask.
         # This makes sure that an image with transparent background
         # won't show up with a white background
-        background_image.paste(program_title_image, (150, 40), 
-                               program_title_image)
-        background_image.paste(overview_button_image, (150, 220), 
-                               overview_button_image)
-        background_image.paste(achievements_button_image, (150, 320), 
-                               achievements_button_image)
-        background_image.paste(completed_button_image, (150, 420), 
-                               completed_button_image)
-        background_image.paste(credits_button_image, (150, 520), 
-                               credits_button_image)
+        self.background_image.paste(self.program_title_image, (150, 40), 
+                               self.program_title_image)
+        self.background_image.paste(self.overview_button_image, (150, 220), 
+                               self.overview_button_image)
+        self.background_image.paste(self.achievements_button_image, (150, 320), 
+                               self.achievements_button_image)
+        self.background_image.paste(self.completed_button_image, (150, 420), 
+                               self.completed_button_image)
+        self.background_image.paste(self.credits_button_image, (150, 520), 
+                               self.credits_button_image)
 
         # Convert the Image object into a TkPhoto object
 
         # Must use self.tk_background_image to save a
         # reference to the image, otherwise this variable
         # will be garbage collected once the class is exited
-        self.tk_background_image = ImageTk.PhotoImage(background_image)
+        self.tk_background_image = ImageTk.PhotoImage(self.background_image)
+    
+    def on_click(self, event):
+        """Takes action depending on where the user clicked"""
+        print("area clicked was", event.x, event.y, sep=" ")
+        #if "Overview" was clicked
+        if 75 <= event.x <= 240 and 220 <= event.y <= 265:
+            print("clicked overview")
+            self.change_button_to_red("Overview")
+        #if Achievements was clicked 
+        elif 75 <= event.x <= 300 and 320 <= event.y <= 365:
+            print("clicked achievements")
+            self.change_button_to_red("Achievements")
+        #if Completed was clicked
+        elif 75 <= event.x <= 240 and 420 <= event.y <= 465:
+            print("clicked completed")
+            self.change_button_to_red("Completed")
+        #if Credits was clicked
+        elif 75 <= event.x <= 197 and 520 <= event.y <= 565:
+            print("clicked credits")
+            self.change_button_to_red("Credits")
 
+    def change_button_to_red(self, button_name):
+        """Updates background_image so that passed in button will flicker
+        red to indicate the user has clicked on that button.
+        """
+        # Red button will be placed over a copy of the background image
+        self.background_image_clicked = self.background_image.copy()
+
+        if button_name == "Overview":
+            self.background_image_clicked.paste(self.overview_red_button_image,
+                                       (150, 220), 
+                                       self.overview_red_button_image)
+        elif button_name == "Achievements":
+            self.background_image_clicked.paste(self.achievements_red_button_image,
+                                       (150, 320), 
+                                       self.achievements_red_button_image)
+        elif button_name == "Completed":
+            self.background_image_clicked.paste(self.completed_red_button_image,
+                                       (150, 420), 
+                                       self.completed_red_button_image)
+        elif button_name == "Credits":
+            self.background_image_clicked.paste(self.credits_red_button_image,
+                                       (150, 520), 
+                                       self.credits_red_button_image)
+
+        self.tk_background_clicked = ImageTk.PhotoImage(self.background_image_clicked)
+        #switch images
+        self.main_menu_label.configure(image=
+                                       self.tk_background_clicked)
+        self.main_menu_label.bind("<ButtonRelease-1>", lambda event: 
+                                  self.main_menu_label.configure(image=
+                                  self.tk_background_image))
 
 class Overview(tk.Frame):
     def __init__(self, parent, controller):
